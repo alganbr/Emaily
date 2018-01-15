79dd41e7fbf627225f9ac2a25a39fc1d5a027710
alganbr <algan_br@hotmail.com>
2018-01-14 18:16:08 -0800

    Implement cookie based authentication for user

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
+// import npm libraries
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport")
+
+// import local files
 const keys = require("./config/keys");
-require("./models/User")
+require("./models/User");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    // cookie session expire in 30 days
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Authenticatino routes handling
 require("./routes/authRoutes")(app);
--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,18 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 
+// store user id in the session in the session
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// use user id to get the user object
+passport.deserializeUser((userId, done) => {
+  User.findById(userId).then(user => {
+    done(null, user);
+  });
+});
+
 // console.developers.google.com
 passport.use(
   new GoogleStrategy(
@@ -14,7 +26,15 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );

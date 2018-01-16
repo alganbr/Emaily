094a81597119b33e8ec4cc653038cdb034711cca
alganbr <algan_br@hotmail.com>
2018-01-15 16:29:13 -0800

    Deployment part 1

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
-const passport = require("passport")
+const passport = require("passport");
+const bodyParser = require("body-parser");
 
 // import local files
 const keys = require("./config/keys");
@@ -13,19 +14,34 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// Wire up middlewares
+app.use(bodyParser.json());
 app.use(
   cookieSession({
-    // cookie session expire in 30 days
+    // cookie session expires in 30 days
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Authenticatino routes handling
+// Route handlers
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
